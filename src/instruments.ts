f6177e57a5b2f087fa222c6fdc15fc0c1ada82f2
vilmos45 <alich.vilmos@gmail.com>
2026-09-14 17:49:06 +0200

    optimised the code, improved README

---
+++ src/instruments.ts
@@ -0,0 +1,11 @@
+import * as Tone from "tone";
+export {/*AMSynth, DuoSynth, FMynth, membraneSynth, metalSynth, noiseSynth, pluckSynth, */synth};
+
+/** /const AMSynth = new Tone.AMSynth().toDestination();
+const DuoSynth = new Tone.DuoSynth().toDestination();
+const FMynth = new Tone.FMSynth().toDestination();
+const membraneSynth = new Tone.MembraneSynth().toDestination();
+const metalSynth = new Tone.MetalSynth().toDestination();
+const noiseSynth = new Tone.NoiseSynth().toDestination();
+const pluckSynth = new Tone.PluckSynth().toDestination();/**/
+const synth: Tone.PolySynth = new Tone.PolySynth();

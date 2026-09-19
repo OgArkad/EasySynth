4075bdcf00e272b1bfdf55aae52b7ddfc82ec364
OgArkad <mrbro19352@proton.me>
2026-09-19 14:53:12 +0200

    made the piano

--- src/oscilloscope.js
+++ src/oscilloscope.js
@@ -3,19 +3,15 @@ const canvas = document.getElementById("oscilloscope-canvas");
 const ctx = canvas.getContext("2d");
 function drawOscilloscope() {
     requestAnimationFrame(drawOscilloscope);
-    // Get current audio signal buffer values (-1 to 1)
     const values = waveform.getValue();
-    // Clear previous frame
     ctx.fillStyle = "#121212";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    // Draw waveform path
     ctx.lineWidth = 2;
-    ctx.strokeStyle = "#00ffcc"; // Hardware glowing color
+    ctx.strokeStyle = "#00ffcc";
     ctx.beginPath();
     const sliceWidth = canvas.width / values.length;
     let x = 0;
     for (let i = 0; i < values.length; i++) {
-        // Map audio value range [-1, 1] to canvas height [0, canvas.height]
         const v = values[i];
         const y = ((v + 1) / 2) * canvas.height;
         if (i === 0) {
@@ -28,6 +24,5 @@ function drawOscilloscope() {
     }
     ctx.stroke();
 }
-// Start visualizer loop
 drawOscilloscope();
 //# sourceMappingURL=oscilloscope.js.map
---
+++ src/piano.js
@@ -0,0 +1,6 @@
+for (let white = 0; white < 16; white++) {
+    let whiteNote = document.createElement("img");
+    whiteNote.src = "media/whtie.png";
+}
+export {};
+//# sourceMappingURL=piano.js.map
